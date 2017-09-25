e44e9f9fb493d0d95c88e383220b94d7e14042aa
Alexis Aubry <me@alexaubry.fr>
2017-09-25 15:27:05 +0200

    Do not include loader for unrelated SSL components

--- Sources/CryptoSupport/CryptoProvider.swift
+++ Sources/CryptoSupport/CryptoProvider.swift
@@ -13,9 +13,6 @@ public class CryptoProvider {
 
     public enum Component: Int {
 
-        /// The SSL library.
-        case ssl
-
         /// The OpenSSL digests.
         case digests
 
@@ -25,18 +22,13 @@ public class CryptoProvider {
         /// The error descriptions for the Crypto APIs.
         case cryptoErrorStrings
 
-        /// The error descriptions for the SSL APIs.
-        case sslErrorStrings
-
         /// Loads the component.
         func load() {
 
             switch self {
-            case .ssl: SSL_library_init()
             case .digests: OpenSSL_add_all_digests()
             case .ciphers: OpenSSL_add_all_ciphers()
             case .cryptoErrorStrings: ERR_load_crypto_strings()
-            case .sslErrorStrings: ERR_load_SSL_strings()
             }
 
         }
@@ -52,10 +44,6 @@ public class CryptoProvider {
      */
 
     public static func load(_ components: Component...) {
-
-        guard components.count > 0 else {
-            return
-        }
 
         for component in components {
 
